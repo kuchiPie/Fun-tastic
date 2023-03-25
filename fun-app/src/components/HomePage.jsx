365936ddcdb6c9e69db630dae67e5efbedecb792
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2023-03-26 01:00:15 +0530

    Update Pages And Routes

---
+++ fun-app/src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Banner from './Banner'
+const HomePage = () => {
+  return (
+    <div className="w-screen flex justify-center">
+        <Banner />
+      </div>
+  )
+}
+
+export default HomePage
--- fun-app/src/components/Marketplace.jsx
+++ fun-app/src/components/Marketplace.jsx
@@ -4,6 +4,7 @@ import {dummyCourseData} from "../utils/dummyCourse"
 const Marketplace = () => {
   return (
     <>
+      <h1 class="mb-4 text-4xl text-center mt-3 font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-black">Available Courses</h1>
       <div class="container my-12 mx-auto px-4 md:px-12">
         <div class="flex flex-wrap -mx-1 lg:-mx-4">
             {dummyCourseData.map((item,i)=>{
