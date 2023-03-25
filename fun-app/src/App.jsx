bd2796cd6047426969725c1d87a02280d8683931
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2023-03-25 19:54:35 +0530

    Home and Market Place page

--- fun-app/src/App.jsx
+++ fun-app/src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import JoinRoom from './Components/JoinRoom'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from "react";
+import Banner from "./components/Banner";
+import Navbar from "./Components/Navbar";
+import Footer from "./components/Footer";
+import Marketplace from "./components/Marketplace";
+const App = () => {
   return (
-    <div className="App">
-    <JoinRoom/>
-    </div>
-  )
-}
+    <>
+      <Navbar />
+      <div className="w-screen flex justify-center">
+        <Banner />
+       </div>
+       <Marketplace/>
+       <Footer/>
+    </>
+  );
+};
 
-export default App
+export default App;
